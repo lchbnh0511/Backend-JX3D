4672e580ded276829923df2668f49a553dd1accc
lchbnh0511 <lechibinh0511@gmail.com>
2026-08-17 16:40:31 +0700

    Add helper FileNameHash

--- Core/Utils/HashHelper.cs
+++ Core/Utils/HashHelper.cs
@@ -17,5 +17,32 @@ namespace BackendJX3D.Core.Utils
 
             return sb.ToString();
         }
+        
+        public static uint FileNameHash(string? pString)
+        {
+            if (string.IsNullOrEmpty(pString))
+                return 0x12345678;
+
+            uint id = 0;
+
+            for (int i = 0; i < pString.Length; i++)
+            {
+                uint c = pString[i];
+
+                if (c >= 'A' && c <= 'Z')
+                    c += 0x20;
+                else if (c == '/')
+                    c = '\\';
+
+                id = (id + (uint)((i + 1) * c)) % 0x8000000B;
+
+                unchecked
+                {
+                    id *= 0xFFFFFFEF;
+                }
+            }
+
+            return id ^ 0x12345678;
+        }
     }
 }
